096295c2878ec69a99acc4e4a1890a135799d9e1
acherry125 <acherry125@gmail.com>
2016-01-31 11:34:52 -0500

    wut

--- 3700bridge.py
+++ 3700bridge.py
@@ -12,7 +12,7 @@ def pad(name):
         result += '\0'
     return result
 
-
+# file stuff
 if __name__ == "__main__":
     id = sys.argv[1]
     LAN = sys.argv[2:]
@@ -20,18 +20,19 @@ if __name__ == "__main__":
 
     # creates sockets and connects to them
     for x in range(len(LAN)):
-        s = socket.socket(socket.AF_UNIX, socket.SOCK_SEQPACKET)
-        s.connect(pad(LAN[x]))
-        sockets.append(s)
+       s = socket.socket(socket.AF_UNIX, socket.SOCK_SEQPACKET)
+       s.connect(pad(LAN[x]))
+       sockets.append(s)
 
     print "Bridge " + id + " starting up\n"
 
     # Main loop
     while True:
         # Calls select with all the sockets; change the timeout value (1)
+        # ready is a list of sockets ready to read
         ready, ignore, ignore2 = select.select(sockets, [], [], 1)
 
-        # Reads from each fo the ready sockets
+        # Reads from each of the ready sockets
         for x in ready:
             data = x.recv(1500)
             print(data)
