dad2cf9d18dfa115b1637e568dda76e567a323bb
acherry125 <acherry125@gmail.com>
2016-01-31 22:08:39 -0500

    Turned main into function
    To execute, in terminal: "chmod 755 3700bridge.py" then remove the
    extension to use with run

--- 3700bridge.py
+++ 3700bridge.py
@@ -4,36 +4,37 @@ import sys
 import socket
 import select
 
-
 # pads the name with null bytes at the end
 def pad(name):
-    result = '\0' + name
-    while len(result) < 108:
-        result += '\0'
-    return result
+        result = '\0' + name
+        while len(result) < 108:
+                result += '\0'
+        return result
 
-# file stuff
+def main(argv):
+        id = argv[1]
+        LAN = argv[2:]
+        sockets = []
+
+        # creates sockets and connects to them
+        for x in range(len(LAN)):
+                s = socket.socket(socket.AF_UNIX, socket.SOCK_SEQPACKET)
+                s.connect(pad(LAN[x]))
+                sockets.append(s)
+
+
+        print "Bridge " + id + " starting up\n"
+
+        # Main loop
+        while True:
+                # Calls select with all the sockets; change the timeout value (1)
+                ready, ignore, ignore2 = select.select(sockets, [], [], 1)
+
+                # Reads from each fo the ready sockets
+                for x in ready:
+                        data = x.recv(1500)
+                        print(data)
+
+
 if __name__ == "__main__":
-    id = sys.argv[1]
-    LAN = sys.argv[2:]
-    sockets = []
-
-    # creates sockets and connects to them
-    for x in range(len(LAN)):
-       s = socket.socket(socket.AF_UNIX, socket.SOCK_SEQPACKET)
-       s.connect(pad(LAN[x]))
-       sockets.append(s)
-
-    print "Bridge " + id + " starting up\n"
-    #change
-
-    # Main loop
-    while True:
-        # Calls select with all the sockets; change the timeout value (1)
-        # ready is a list of sockets ready to read
-        ready, ignore, ignore2 = select.select(sockets, [], [], 1)
-
-        # Reads from each of the ready sockets
-        for x in ready:
-            data = x.recv(1500)
-            print(data)
+	main(sys.argv)
