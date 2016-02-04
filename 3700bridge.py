7ba2ae3b0b6bd5bde8fc6cf65a733179a9110d2a
acherry125 <acherry125@gmail.com>
2016-02-03 20:20:09 -0500

    Revert "bad"
    This reverts commit 372183ab561ccc7b5ce9b64e130b107f81f0cd8c.

--- 3700bridge.py
+++ 3700bridge.py
@@ -3,96 +3,38 @@
 import sys
 import socket
 import select
-import json
 
 # pads the name with null bytes at the end
 def pad(name):
-    result = '\0' + name
-    while len(result) < 108:
-        result += '\0'
+        result = '\0' + name
+        while len(result) < 108:
+                result += '\0'
         return result
 
 def main(argv):
-    if len(argv) < 2:
-        raise ValueError('Must be supplied an ID and LAN(\'s)')
+        id = argv[1]
+        LAN = argv[2:]
+        sockets = []
 
-    id = argv[0]
-    root = id
+        # creates sockets and connects to them
+        for x in range(len(LAN)):
+                s = socket.socket(socket.AF_UNIX, socket.SOCK_SEQPACKET)
+                s.connect(pad(LAN[x]))
+                sockets.append(s)
 
 
-    LAN = argv[2:]
-    sockets = []
+        print "Bridge " + id + " starting up\n"
 
-    # creates sockets and connects to them
-    for x in range(len(LAN)):
-        s = socket.socket(socket.AF_UNIX, socket.SOCK_SEQPACKET)
-        s.connect(pad(LAN[x]))
-        sockets.append(s)
+        # Main loop
+        while True:
+                # Calls select with all the sockets; change the timeout value (1)
+                ready, ignore, ignore2 = select.select(sockets, [], [], 1)
 
-    print "Bridge " + id + " starting up\n"
-
-    # Main loop
-    while True:
-        # Calls select with all the sockets; change the timeout value (1)
-        ready, ignore, ignore2 = select.select(sockets, [], [], 1)
-
-    # Reads from each fo the ready sockets
-    for x in ready:
-        data = x.recv(1500)
-        data = json.loads(data)
-        type = data['type']
-        print(type)
+                # Reads from each fo the ready sockets
+                for x in ready:
+                        data = x.recv(1500)
+                        print(data)
 
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
-
-# #!/usr/bin/python -u
-# # The -u makes output unbuffered, so it will show up immediately
-# import sys
-# import socket
-# import select
-# import json
-#
-# # pads the name with null bytes at the end
-# def pad(name):
-#     result = '\0' + name
-#     while len(result) < 108:
-#         result += '\0'
-#         return result
-#
-# def main(argv):
-#     if len(argv) < 2:
-#         raise ValueError('Must be supplied an ID and LAN(\'s)')
-#
-#     id = argv[0]
-#     root = id
-#     LAN = argv[1:]
-#     sockets = []
-#     print LAN
-#     # creates sockets and connects to them
-#     for x in range(len(LAN)):
-#         s = socket.socket(socket.AF_UNIX, socket.SOCK_SEQPACKET)
-#         s.connect(pad(LAN[x]))
-#         sockets.append(s)
-#
-#     print "Bridge " + id + " starting up\n"
-# """
-#     # Main loop
-#     while True:
-#         # Calls select with all the sockets; change the timeout value (1)
-#         ready, ignore, ignore2 = select.select(sockets, [], [], 1)
-#
-#     # Reads from each fo the ready sockets
-#     for x in ready:
-#         data = x.recv(1500)
-# 	print data
-#         #data = json.loads(data)
-#         #type = data['type']
-#         #print(type)
-# """
-#
-#
-# if __name__ == "__main__":
-# 	#print sys.argv[1:]
-# 	main(sys.argv[1:])
+	main(sys.argv)
